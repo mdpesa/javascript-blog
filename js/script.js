de912c5a1e2b3c044a775cc7882bb5c32dbeac6b
Marta Santos <mdpesa@gmail.com>
2022-11-11 17:05:59 +0100

    add class 'active' to the correct article

--- js/script.js
+++ js/script.js
@@ -15,22 +15,23 @@ const titleClickHandler = function (event) {
 	for (let activeLink of activeLinks) {
 		activeLink.classList.remove('active');
 	}
-	/* [IN PROGRESS] add class 'active' to the clicked link */
+	/* [DONE] add class 'active' to the clicked link */
 	clickedElement.classList.add('active');
 	console.log('clickedElement:', clickedElement);
-	/* [done] remove class 'active' from all articles */
+	/* [DONE] remove class 'active' from all articles */
 	const activeArticles = document.querySelectorAll('.post');
 
 	for (let activeArticle of activeArticles) {
 		activeArticle.classList.remove('active');
 	}
-	/* get 'href' attribute from the clicked link */
+	/* [DONE] get 'href' attribute from the clicked link */
 	const articleSelector = clickedElement.getAttribute('href');
 	console.log(articleSelector);
-	/* find the correct article using the selector (value of 'href' attribute) */
+	/* [DONE] find the correct article using the selector (value of 'href' attribute) */
 	const targetArticle = document.querySelector(articleSelector);
 	console.log(targetArticle);
-	/* add class 'active' to the correct article */
+	/* [DONE] add class 'active' to the correct article */
+	targetArticle.classList.add('active');
 };
 
 const links = document.querySelectorAll('.titles a');
